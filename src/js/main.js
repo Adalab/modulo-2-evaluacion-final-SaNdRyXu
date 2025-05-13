384fe6ddd65e7b134b8a88bedb01750bbde2d584
Sandra <sandra_13_c70@hotmail.com>
2025-05-13 23:25:13 +0200

    Pagina

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,20 @@
 'use strict';
+// Constantes
+const btn = document.querySelector('.js-button');
+const input = document.querySelector('.js-input');
+const url = 'https://fakestoreapi.com/products';
+
+//variables
+
+//funciones 
+
+//funciones manejadoras
+
+
+//Almacenar informacion en el local storage
+
+
+ //mandar datos a la API
+
+
+ //eventos
