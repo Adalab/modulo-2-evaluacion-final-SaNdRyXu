c5a58e857ee7e053b3a5f3763af61b020c0f7d8a
Sandra <sandra_13_c70@hotmail.com>
2025-05-15 12:04:19 +0200

    avanzamos

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,61 @@
 'use strict';
+
+
+
 // Constantes
 const btn = document.querySelector('.js-button');
 const input = document.querySelector('.js-input');
+const listProducts = document.querySelector('.js-list-products');
 const url = 'https://fakestoreapi.com/products';
+
+
 
 //variables
 
+let products = [];
+let cart = [];
+
+
 //funciones 
+//pintar los productos en el HTML
+
+   function renderProducts(products) {
+    for (let product of products) {
+        
+
+        const boxProduct = document.createElement('div');
+        boxProduct.className = 'boxProduct';
+
+        const img = document.createElement('img');
+        img.src = product.image;
+        img.alt = product.title;
+        img.className = 'img';
+
+        const h3 = document.createElement('h3');
+        h3.innerText = product.title;
+
+    
+        
+        const price = document.createElement('p');
+        price.innerText = product.price + '€';
+
+        const btnBuy = document.createElement('button');
+        btnBuy.innerText = 'Buy';
+        btnBuy.className = 'btn-buy';
+
+        
+        
+        boxProduct.appendChild(img);
+        boxProduct.appendChild(h3);
+        boxProduct.appendChild(btnBuy);
+        boxProduct.appendChild(price);
+        listProducts.appendChild(boxProduct);
+    }
+}
+
+
+
+
 
 //funciones manejadoras
 
@@ -18,3 +67,12 @@ const url = 'https://fakestoreapi.com/products';
 
 
  //eventos
+
+ fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+        products = data;
+        console.log(products);
+        renderProducts(products);
+    });
+
